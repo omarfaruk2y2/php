1df99218fca4ca35fca1efe4402620b3aa8d66b3
omarfaruk2y2 <omarfaruk2y2@gmail.com>
2021-11-23 02:33:19 +0600

    last commit

---
+++ forloop.php
@@ -0,0 +1,14 @@
+<?php
+for($a=1; $a<=100; $a=$a+10){
+    for($b=$a; $b<$a+10; $b++){
+        echo $b . " ";
+    }
+    echo "<br>";
+}
+
+echo "<br><br><br>Love Code <br><br><br>";
+
+for($a=0; $a<=100; $a=$a+3){
+    echo "$a <br>";
+}
+
